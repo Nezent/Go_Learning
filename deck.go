6d0ccfdc0eea991186846dafe1d3653e50ab3961
Sirajum Munir <nezentbd@gmail.com>
2023-12-26 19:22:16 +0600

    Saving to a file

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -21,4 +26,42 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck,handSize int) (deck,deck) {
+	return d[:handSize],d[handSize:]
+}
+
+func (d deck) writeIntoFile(filename string) {
+
+	// Open the file for writing, create it if it doesn't exist
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a bufio.Writer for efficient writing
+	writer := bufio.NewWriter(file)
+
+	// Write the string to the file
+	_, err = writer.WriteString(d.toString())
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	// Flush the bufio.Writer to ensure all data is written to the file
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println("Error flushing writer:", err)
+		return
+	}
+
+	fmt.Println("String has been successfully written to", filename)
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d),",")
 }
