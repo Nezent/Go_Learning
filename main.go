639fd0c981967d8506c840d8a498d2b05e409953
Sirajum Munir <nezentbd@gmail.com>
2024-01-03 19:05:20 +0600

    Go multi threading option v2

--- main.go
+++ main.go
@@ -12,16 +12,25 @@ func main() {
 		"http://google.com",
 		"http://golang.org",
 	}
+
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link,c)
+	}
+	
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<- c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link,"might be down!")
+		c <- "Might be down I think"
 		return
 	}
 	fmt.Println(link,"is up!")
+	c <- "Yup! its up"
 }
